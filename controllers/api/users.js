99ca81e190ced8de5cc5ce8c16bf339d10f28af2
xandramiyo <aj.sumiyoshi@gmail.com>
2023-03-03 10:57:24 -0800

    removes checkToken

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -31,11 +31,6 @@ async function login(req, res) {
     }
 }
 
-function checkToken(req, res) {
-    console.log('req.user', req.user)
-    res.json(req.exp)
-}
-
 /*-- Helper Functions --*/
 
 function createJWT(user) {
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,5 @@ const ensureLoggedIn = require('../../config/ensureLoggedIn')
 // POST /api/users
 router.post('/', usersCtrl.create);
 router.post('/login', usersCtrl.login);
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
 
 module.exports = router;
